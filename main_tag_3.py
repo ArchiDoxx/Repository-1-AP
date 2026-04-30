31b2c1461fe40ea5b47143554fce613ab68d1102
Lucas Vöhringer <luce.buum@gmail.com>
2026-04-30 11:06:08 +0200

    Arbeit an tag 3

--- main_tag_3.py
+++ main_tag_3.py
@@ -4,4 +4,14 @@ app = fapi()
 
 @app.get("/queryparameters")
 def get_query_parameters(param1: str, param2: int) -> dict:
-        return {"param1": param1, "param2": param2}
+
+        namen = ['martin', 'michael', 'sarah', 'anna', 'tom', 'lisa']
+
+        if not param1:
+              return{"namen": namen}
+        
+        namen_gefiltert = []
+        for name in namen:
+                if param1 in name:
+                 namen_gefiltert.append(name)
+        return {"param1": param1, "param2": param2, "filtered_names": namen_gefiltert}
