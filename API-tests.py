22624c4366d286366d55cab9147587ecf0390c96
Lucas Vöhringer <luce.buum@gmail.com>
2026-04-30 12:10:51 +0200

    kp

---
+++ API-tests.py
@@ -0,0 +1,29 @@
+from main import app
+import requests
+
+url = "http://127.0.0.1:8000"
+
+def test_get_root():
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("GET / - SUCCESS")
+    else:
+        print("GET / - FAILED")
+
+
+if __name__ == "__main__":
+    test_get_root()
+
+def test_post_creation():
+    payload = {
+        "title": "title",
+        "content": "content",
+        "category": "category",
+        "tags": ["tag1", "tag2"]
+    }
+    response = requests.post(f"{url}/create", json=payload)
+    if response.status_code == 200:
+        print("POST /create - SUCCESS")
+    else:       
+        print("POST /create - FAILED")
+    
